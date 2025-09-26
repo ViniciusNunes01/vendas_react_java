00e71009f693d7eb04d94f1c4a8a7923199949e2
ViniciusNunes01 <sanjip415@gmail.com>
2025-09-25 22:59:59 -0300

    metodo de atualizar produto

--- backend/vendas-api/src/main/java/io/github/viniciusnunes01/vendasapi/rest/produtos/ProdutoController.java
+++ backend/vendas-api/src/main/java/io/github/viniciusnunes01/vendasapi/rest/produtos/ProdutoController.java
@@ -1,8 +1,13 @@
 package io.github.viniciusnunes01.vendasapi.rest.produtos;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,4 +33,18 @@ public class ProdutoController {
 		return ProdutoFormRequest.fromModel(entidadeProduto);
 	}
 
+	@PutMapping("{id}")
+	public ResponseEntity<Void> atualizar(@PathVariable Long id, @RequestBody ProdutoFormRequest produto) {
+
+		Optional<Produto> produtoExistente = repository.findById(id);
+		if (produtoExistente.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+
+		Produto entidade = produto.toModel();
+		entidade.setId(id);
+		repository.save(entidade);
+
+		return ResponseEntity.ok().build();
+	}
 }
