a63f085a2948fad2e0baf44605b00744a8b7a4e3
ViniciusNunes01 <sanjip415@gmail.com>
2025-10-02 18:19:41 -0300

    metodo listagem produtos

--- backend/vendas-api/src/main/java/io/github/viniciusnunes01/vendasapi/rest/produtos/ProdutoController.java
+++ backend/vendas-api/src/main/java/io/github/viniciusnunes01/vendasapi/rest/produtos/ProdutoController.java
@@ -1,10 +1,13 @@
 package io.github.viniciusnunes01.vendasapi.rest.produtos;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -46,5 +49,11 @@ public class ProdutoController {
 		repository.save(entidade);
 
 		return ResponseEntity.ok().build();
+	}
+
+	@GetMapping
+	public List<ProdutoFormRequest> getLista() {
+
+		return repository.findAll().stream().map(ProdutoFormRequest::fromModel).collect(Collectors.toList());
 	}
 }
