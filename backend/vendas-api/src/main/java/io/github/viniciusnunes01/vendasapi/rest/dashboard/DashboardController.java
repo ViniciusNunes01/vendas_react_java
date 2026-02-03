c896a4d23afaff7b1306fbc9b990d900b73af85c
ViniciusNunes01 <sanjip415@gmail.com>
2026-02-03 19:45:52 -0300

    dashboard api feita

---
+++ backend/vendas-api/src/main/java/io/github/viniciusnunes01/vendasapi/rest/dashboard/DashboardController.java
@@ -0,0 +1,35 @@
+package io.github.viniciusnunes01.vendasapi.rest.dashboard;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.github.viniciusnunes01.vendasapi.model.repository.ClienteRepository;
+import io.github.viniciusnunes01.vendasapi.model.repository.ProdutoRepository;
+import io.github.viniciusnunes01.vendasapi.model.repository.VendaRepository;
+
+@RestController
+@RequestMapping("/api/dashboard")
+public class DashboardController {
+
+	@Autowired
+	private VendaRepository vendas;
+
+	@Autowired
+	private ClienteRepository clientes;
+
+	@Autowired
+	private ProdutoRepository produtos;
+
+	@GetMapping
+	public DashboardData getDashBoard() {
+
+		long vendasCount = vendas.count();
+		long clientesCount = clientes.count();
+		long produtosCount = produtos.count();
+
+		return new DashboardData(produtosCount, clientesCount, vendasCount);
+	}
+
+}
