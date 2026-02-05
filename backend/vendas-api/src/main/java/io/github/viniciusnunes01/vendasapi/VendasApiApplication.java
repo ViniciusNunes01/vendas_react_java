1fc076cfd17de2d33a20e9250ac84b78d74e2a1b
ViniciusNunes01 <sanjip415@gmail.com>
2026-02-05 20:29:13 -0300

    criando outros ambientes, como homologação e produção

--- backend/vendas-api/src/main/java/io/github/viniciusnunes01/vendasapi/VendasApiApplication.java
+++ backend/vendas-api/src/main/java/io/github/viniciusnunes01/vendasapi/VendasApiApplication.java
@@ -1,20 +1,33 @@
 package io.github.viniciusnunes01.vendasapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class VendasApiApplication {
 
-	@GetMapping("/hello/{nome}")
-	public String hello(@PathVariable String nome) {
-		return "Hello " + nome;
+	@Autowired
+	private Environment env;
+
+	@GetMapping("/")
+	private String getAmbiente() {
+
+		String ambienteAtual = "PADRÃO (nenhum) ";
+
+		if (env.getActiveProfiles().length > 0) {
+			ambienteAtual = env.getActiveProfiles()[0];
+		}
+
+		String appName = env.getProperty("spring.application.name");
+
+		return String.format("Ambiente: %s | App Name: %s", ambienteAtual, appName);
 	}
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApiApplication.class, args);
 	}
